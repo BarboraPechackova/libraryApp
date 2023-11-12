ca5097073a9d1048b58185ed2986690d1ce20fd6
Stanislav Sadílek <sadilsta@fel.cvut.cz>
2023-11-12 15:27:28 +0100

    Added more tests for BookService

--- src/main/java/cz/cvut/fel/ear/library/service/BookService.java
+++ src/main/java/cz/cvut/fel/ear/library/service/BookService.java
@@ -1,15 +1,18 @@
 package cz.cvut.fel.ear.library.service;
 
 import cz.cvut.fel.ear.library.dao.BookDao;
+import cz.cvut.fel.ear.library.dao.BookLoanDao;
+import cz.cvut.fel.ear.library.exceptions.BookIsAlreadyLoanedException;
+import cz.cvut.fel.ear.library.exceptions.BookIsNotReturnedException;
 import cz.cvut.fel.ear.library.exceptions.InvalidArgumentException;
 import cz.cvut.fel.ear.library.model.Book;
+import cz.cvut.fel.ear.library.model.BookLoan;
 import cz.cvut.fel.ear.library.model.BookState;
 import cz.cvut.fel.ear.library.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,10 +20,12 @@ import java.util.Objects;
 public class BookService {
 
     private final BookDao dao;
+    private final BookLoanDao bookLoanDao;
 
     @Autowired
-    public BookService(BookDao dao) {
+    public BookService(BookDao dao, BookLoanDao bookLoanDao) {
         this.dao = dao;
+        this.bookLoanDao = bookLoanDao;
     }
 
     @Transactional(readOnly = true)
@@ -34,21 +39,21 @@ public class BookService {
     }
 
     @Transactional
-    public void persist(Book book) throws InvalidArgumentException {
+    public void persist(Book book) {
         Objects.requireNonNull(book);
         validate(book);
         dao.persist(book);
     }
 
     @Transactional
-    public void persistAll(List<Book> books) throws InvalidArgumentException {
+    public void persistAll(List<Book> books) {
         for (Book b : books) {
             persist(b);
         }
     }
 
     @Transactional
-    public void update(Book book) throws InvalidArgumentException {
+    public void update(Book book) {
         Objects.requireNonNull(book);
         validate(book);
         dao.update(book);
@@ -57,7 +62,13 @@ public class BookService {
     @Transactional
     public List<Book> findAllFromUser(User user) {
         Objects.requireNonNull(user);
-        return dao.findAll(user);
+        return dao.findAllFromUser(user);
+    }
+
+    @Transactional
+    public List<Book> findAllVisibleFromUser(User user) {
+        Objects.requireNonNull(user);
+        return dao.findAllFromUser(user,true);
     }
 
     @Transactional
@@ -97,14 +108,44 @@ public class BookService {
 
     // TODO dalsi metody, upravit ty co uz mam jestli ukazovat visible only
 
-    public void findBooksByState() {}
-    public void findAllVisible() {}
+    public List<Book> findByName(String name) {
+        Objects.requireNonNull(name);
+        return dao.findByName(name);
+    }
 
-    public void removeBooksFromUser (User user) {}
+    public List<Book> findVisibleByName(String name) {
+        Objects.requireNonNull(name);
+        return dao.findByName(name,true);
+    }
 
-    private void validate(Book book) throws InvalidArgumentException {
+    public void findBooksByState(BookState state) {}
+
+    public List<Book> findAllVisible() {
+        return dao.findAllVisible(true);
+    }
+
+    public void removeBooksFromUser (User user) {
+        validateUserRemove(user);
+    }
+
+    private void validate(Book book) {
         if (book.getPrice() < 0) {
             throw new InvalidArgumentException("Price must not be negative");
+        }
+    }
+
+    private void validateUserRemove(User user) {
+        List<Book> userBooks = findAllFromUser(user);
+        for (Book book : userBooks) {
+            if (book.getState() == BookState.VYPUJCENA) {
+                throw new BookIsAlreadyLoanedException("Remove user failed! Book " + book.getName() + " is lent.");
+            }
+        }
+        List<BookLoan> userLoans = bookLoanDao.getUserLoans(user);
+        for (BookLoan bookLoan : userLoans) {
+            if (!bookLoan.isReturned()) {
+                throw new BookIsNotReturnedException("Remove user failed! Book " + bookLoan.getBook().getName() + " is not returned.");
+            }
         }
     }
 }
--- src/test/java/cz/cvut/fel/ear/library/service/BookServiceTest.java
+++ src/test/java/cz/cvut/fel/ear/library/service/BookServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -26,12 +27,20 @@ public class BookServiceTest {
     private BookService service;
 
     @Test
-    public void persistPersistsBook() throws InvalidArgumentException {
+    public void persistPersistsBook() {
         final Book book = new Book();
         em.persist(book);
         service.persist(book);
 
         assertEquals(1,service.findAll().size());
+    }
+
+    @Test
+    public void persistWithNegativePriceThrowsInvalidArgument() throws InvalidArgumentException {
+        final Book book = new Book();
+        book.setPrice(-1);
+        em.persist(book);
+        assertThrows(InvalidArgumentException.class, () -> service.persist(book));
     }
 
     @Test
@@ -49,6 +58,73 @@ public class BookServiceTest {
         assertEquals(2,service.findAll().size());
         assertEquals(1,service.findAllFromUser(user).size());
     }
+
+    @Test
+    public void findAllVisibleFromUserReturnsOnlyVisible() {
+        final User user = new User();
+        final Book b1 = new Book();
+        final Book b2 = new Book();
+        b1.setUser(user);
+        b1.setVisible(true);
+
+        b2.setUser(user);
+        b2.setVisible(false);
+
+        em.persist(user);
+        em.persist(b1);
+        em.persist(b2);
+        service.persistAll(List.of(b1, b2));
+
+        assertEquals(2,service.findAllFromUser(user).size());
+        assertEquals(1,service.findAllVisibleFromUser(user).size());
+    }
+
+    @Test
+    public void findByNameReturnsAllBooksWithSearchedWordInName() {
+        final Book b1 = new Book();
+        final Book b2 = new Book();
+        final Book b3 = new Book();
+        final Book b4 = new Book();
+
+        b1.setName("name");
+        b2.setName("name bla bla");
+        b3.setName("bla bla name");
+        b4.setName("nothing");
+
+        em.persist(b1);
+        em.persist(b2);
+        em.persist(b3);
+        em.persist(b4);
+
+        service.persistAll(List.of(b1,b2,b3,b4));
+        assertEquals(4,service.findAll().size());
+        assertEquals(3,service.findByName("name").size());
+    }
+
+    @Test
+    public void findVisibleByNameReturnsAllVisibleBooksWithSearchedWordInName() {
+        final Book b1 = new Book();
+        final Book b2 = new Book();
+        final Book b3 = new Book();
+        final Book b4 = new Book();
+
+        b1.setName("name");
+        b1.setVisible(true);
+        b2.setName("name bla bla");
+        b3.setName("bla bla name");
+        b4.setName("nothing");
+
+        em.persist(b1);
+        em.persist(b2);
+        em.persist(b3);
+        em.persist(b4);
+
+        service.persistAll(List.of(b1,b2,b3,b4));
+        assertEquals(4,service.findAll().size());
+        assertEquals(1,service.findVisibleByName("name").size());
+    }
+
+
 
 //    @Test
 //    public void addBook() {
