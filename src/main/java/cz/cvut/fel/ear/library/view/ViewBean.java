3be30d90d041534968ca123f953457123b9dddc7
Burlejan <burlejan@fel.cvut.cz>
2023-12-05 15:34:31 +0100

    First steps to fixing the jsf errors

---
+++ src/main/java/cz/cvut/fel/ear/library/view/ViewBean.java
@@ -0,0 +1,24 @@
+package cz.cvut.fel.ear.library.view;
+
+import java.util.Date;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("view")
+public class ViewBean {
+
+    public ViewBean() {
+        System.out.println("Created!");
+    }
+
+    public String getFrom() {
+        return this.toString();
+    }
+
+    public String getDate() {
+        return new Date().toString();
+    }
+
+}
