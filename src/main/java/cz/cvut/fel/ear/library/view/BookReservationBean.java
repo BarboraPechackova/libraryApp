08e0b87cba74f6cbf48027623ebbb0539c950f53
pechabar <pechabar@cvut.fel.cz>
2023-12-20 00:20:48 +0100

    added redirect to login page for guests, added message to reservation

--- src/main/java/cz/cvut/fel/ear/library/view/BookReservationBean.java
+++ src/main/java/cz/cvut/fel/ear/library/view/BookReservationBean.java
@@ -3,6 +3,8 @@ package cz.cvut.fel.ear.library.view;
 import cz.cvut.fel.ear.library.model.Book;
 import cz.cvut.fel.ear.library.model.User;
 import cz.cvut.fel.ear.library.rest.ReservationController;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
@@ -20,5 +22,6 @@ public class BookReservationBean {
 
     public void createBookReservation(Book book, User user) {
         reservationController.createReservation(book, user);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Rezervace vytvořena"));
     }
 }
